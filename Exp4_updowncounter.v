199f2f0212a57087c6191b8a8d5cad1957bc9f2d
Sowjanya2005 <159280328+Sowjanya2005@users.noreply.github.com>
2024-05-07 18:23:50 +0530

    Update Exp4_updowncounter.v

--- Exp4_updowncounter.v
+++ Exp4_updowncounter.v
@@ -1,9 +1,30 @@
-//4 BIT UP/DOWN COUNTER 
-module updowncounter(clk,rst,ud,out); 
-input clk,rst,ud; 
-output reg[3:0]out = 4’b0000;
+UP COUNTER 
 
-
+module upcounter(clk,rst,count);
+input clk,rst;
+output[3:0]count;
+reg[3:0]count;
+always@(posedge clk)
+begin
+if(rst)
+count<=4'b0;
+else
+count<=count+1;
+end
 endmodule
 
+DOWN COUNTER
+
+module downcounter(clk,rst,count);
+input clk,rst;
+output[3:0]count;
+reg[3:0]count;
+always@(posedge clk)
+begin
+if(rst)
+count<=4'b0;
+else
+count <=count-1;
+end
+endmodule
 
